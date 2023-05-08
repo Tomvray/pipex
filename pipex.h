49f37a9d2fef49e45432661765beef1d8733a2be
Thomas Vray <tvray@shi-r8-p5.s19.be>
2023-05-08 15:48:41 +0200

    .h

--- pipex.h
+++ pipex.h
@@ -5,6 +5,12 @@
 # include <stdlib.h>
 # include "libft/libft.h"
 # include <sys/wait.h>
+# include <errno.h>
 #include <stdio.h>
+
+void	*ft_free_tab(char	**ptr);
+void	ft_exit(char *str, int exit_code);
+char	*ft_path(char	*cmd, char **env);
+void    ft_perror(char *file_name, char **env);
 
 #endif
